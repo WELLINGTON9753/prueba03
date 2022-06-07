d92e5b7b0a1fc3bbe13428e5d1a8ca6e7c4c708d
Diego Tapia <jtapiav4@est.ups.edu.ec>
2022-06-07 18:06:14 -0500

    clases y metodos (modelo)terminados

--- src/main/java/ups/ejercicio_03_03/Model/Departamento.java
+++ src/main/java/ups/ejercicio_03_03/Model/Departamento.java
@@ -9,5 +9,68 @@ package ups.ejercicio_03_03.Model;
  * @author Paul Aguaiza
  */
 public class Departamento {
+    private int codigo;
+    private String Nombre;
+    private Empleado gerente;
+    private Empresa empresa;
+    private String ubicacion;
+
+    public Departamento(int codigo, String Nombre, Empresa empresa, String ubicacion) {
+        this.codigo = codigo;
+        this.Nombre = Nombre;
+        this.empresa = empresa;
+        this.ubicacion = ubicacion;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public Empleado getGerente() {
+        return gerente;
+    }
+
+    
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+   
+    
+    public void asignarGerente(Empleado gerente) {
+        this.gerente = gerente;
+    }
+    
+    public String mostrarInformacion(){
+         if(this.gerente == null){
+            return "Departamento{" + "codigo=" + codigo + ", Nombre=" + Nombre + ", gerente=" + "Sin gerente" + ", empresa=" + empresa + ", ubicacion=" + ubicacion + '}';
+        }
+        return "Departamento{" + "codigo=" + codigo + ", Nombre=" + Nombre + ", gerente=" + gerente + ", empresa=" + empresa + ", ubicacion=" + ubicacion + '}';
+    }
     
 }
--- src/main/java/ups/ejercicio_03_03/Model/Empleado.java
+++ src/main/java/ups/ejercicio_03_03/Model/Empleado.java
@@ -4,10 +4,132 @@
  */
 package ups.ejercicio_03_03.Model;
 
+import java.time.LocalDate;
+
+
 /**
  *
  * @author Paul Aguaiza
  */
 public class Empleado {
+    private String cedula;
+    private String nombre;
+    private LocalDate fechaNacimiento;
+    private String nacionalidad;
+    private String direccion;
+    private String cargo;
+    private float saliario;
+    private LocalDate fechaEntrada;
+    private Departamento departamento;
+
+    public Empleado(String cedula, String nombre, LocalDate fechaNacimiento, String nacionalidad, String direccion, String cargo, float saliario, LocalDate fechaEntrada, Departamento departamento) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.nacionalidad = nacionalidad;
+        this.direccion = direccion;
+        this.cargo = cargo;
+        this.saliario = saliario;
+        this.fechaEntrada = fechaEntrada;
+        this.departamento = departamento;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public float getSaliario() {
+        return saliario;
+    }
+
+    public void setSaliario(float saliario) {
+        this.saliario = saliario;
+    }
+
+    public LocalDate getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public void setFechaEntrada(LocalDate fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    
+
+    @Override
+    public String toString() {
+        return "Empleado{" + "cedula=" + cedula + ", nombre=" + nombre + ", fechaNacimiento=" + fechaNacimiento + ", nacionalidad=" + nacionalidad + ", direccion=" + direccion + ", cargo=" + cargo + ", saliario=" + saliario + ", fechaEntrada=" + fechaEntrada + ", departamento=" + departamento+" ";
+    }
+    
+    public int calcularEdad(int anioActual){
+        int edad = anioActual - this.fechaNacimiento.getYear();
+        return edad;
+    }
+    
+    //antiguedad en la empresa
+    public int calcularAntiguedad(int anioActual){
+        
+        int antiguedad = anioActual - this.fechaEntrada.getYear();
+        return antiguedad;
+    }
+    
+    public void cambiarDepartamento ( Departamento departamento) {
+        this.departamento = departamento;
+    }
+    
+    public String mostrarInformacion(int anioActual){
+        String info = toString() + "\nedad actual empleado: "+calcularEdad(anioActual)+"\nAntiguedad del empleado: "+calcularAntiguedad(anioActual)+" }";
+        return info;
+    }
+    
+    
     
 }
