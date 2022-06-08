133939f73e2b8e5085a732c780489b04b1b03a63
Diego Tapia <jtapiav4@est.ups.edu.ec>
2022-06-07 20:28:46 -0500

    terminadas clases se la capa de Servicio

---
+++ src/main/java/ups/ejercicio_03_03/Service/IDepartamentoService.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ups.ejercicio_03_03.Service;
+
+import java.util.List;
+import ups.ejercicio_03_03.Model.Departamento;
+
+/**
+ *
+ * @author Diego
+ */
+public interface IDepartamentoService {
+    public Departamento crearDepartamento();
+    public List<Departamento> listarDepartementos();
+}
